846a26d1e7a47544583bf0a5c185330c60649634
PinkishFrog4542 <PinkishFrog4542@github.com>
2020-06-19 18:14:46 -0500

    Guess my number

---
+++ chapter4/guess.py
@@ -0,0 +1,39 @@
+# This is a guess the number game
+import random
+
+guessesTaken = 0
+
+print('Hello! What is your name?')
+playername = input()
+
+number = random.randint(1, 20)
+print('Well, ' + playername + ', I am thinking of a number between 1 and 20, You have 6 guesses!')
+
+while guessesTaken < 6:
+    print('Take a guess.')
+    guess = input()
+    guess = int(guess)
+
+    guessesTaken = guessesTaken + 1
+
+    if guess < number:
+        print('Your guess is too low.')
+
+    if guess > number:
+        print('Your guess is too high.')
+
+    if guess == number:
+        guessesTaken = str(guessesTaken)
+        print('Good job, ' + playername + '! You guessed my number in ' +
+                        guessesTaken + ' guesses!')
+
+
+if guess != number:
+        number = str(number)
+        print('Nope. The number I was thinking of was ' + number)
+
+
+
+
+
+
