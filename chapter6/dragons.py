1842770223365e6bb49037457237c40668734960
PinkishFrog4542 <PinkishFrog4542@github.com>
2020-06-23 13:35:25 -0500

    Dragon Realm with comments

--- chapter6/dragons.py
+++ chapter6/dragons.py
@@ -1,17 +1,20 @@
 # Dragon Realm Game
 import random
 import time
-
+#Def statement is for defining that this is the Intro
 def displayIntro():
     print('You are in a land full of dragons. In front of you,')
     print('you see two caves. In one cave, the dragon is friendly')
     print('and will share his treasure with you. The other dragon')
     print('is greedy and hungry, and will eat you on sight.')
     print()
-
+#Def statement to tell that its asking you to choose a cave
 def chooseCave():
     cave = ''
+    #Cave is function here is equal to a blank string
     while cave !='1' and cave !='2':
+        #This function is to make sure the user typed 1 or 2, and not something else.
+        #Also the and is a another type of operator just like or and it is a boolean operator.
         print('Which cave will you go into? (1 or 2)')
         cave = input()
 
@@ -37,10 +40,11 @@ playAgain = 'yes'
 while playAgain == 'yes' or playAgain == 'y':
 
     displayIntro()
-
+#The choose cave function lets the player type in the the cave they want to go into.
     caveNumber = chooseCave()
 
     checkCave(caveNumber)
-
+#This is the function that will display either if it will give you treasure or gobble you up.
     print('Do you want to play again? (yes or no)')
     playAgain = input()
+    #This is the playagain function where you press y or yes to play again or no to stop playing.
