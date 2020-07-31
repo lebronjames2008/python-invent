1fedeede74ced94a59a0c5dccadec80825030c8b
PinkishFrog4542 <PinkishFrog4542@github.com>
2020-07-31 14:26:56 -0500

    Math

---
+++ w3schools/Math.py
@@ -0,0 +1,28 @@
+# The min() and max() functions can be used to find the lowest or highest value in an iterable:
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+# The abs() function returns the absolute (positive) value of the specified number:
+x = abs(-7.25)
+
+print(x)
+
+# The pow(x, y) function returns the value of x to the power of y (xy).
+x = pow(4, 3)
+
+print(x)
+
+
+# Python has also a built-in module called math, which extends the list of mathematical functions.
+import math
+
+x = math.sqrt(64)
+
+print(x)
+# The sqrt just finds the square root of the number you input
+# Math ceil rounds upwards to its nearest integer
+# Math floor round downwards to its nearest integer
+# pi makes the return value as the value of pi
---
+++ w3schools/demo_module1.py
@@ -0,0 +1,10 @@
+import mymodule
+
+mymodule.greeting("Sibi")
+
+person1 = {
+  "name": "Sibi",
+  "age": 11,
+  "country": "United States"
+}
+
